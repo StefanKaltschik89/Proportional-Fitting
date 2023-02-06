58f9465f3c719a7b4d1f3872682a483a83116c9a
Stefan Kaltschik <110018356+StefanKaltschik89@users.noreply.github.com>
2023-02-06 14:42:19 +0100

    Update raking.R

--- raking.R
+++ raking.R
@@ -1,11 +1,11 @@
-#Fill in the name of your dataset instead of "survey" and adapt the variable names (e.g. "gender") to your needs!
+# The name of my dataset was "survey" and i balanced the variables "gender" and "migration". Just adapt the names to your needs.
+
 #---check weights from raw-data
 wpct(survey$gender)
 wpct(survey$migration)
 
-
 #---setting target weights from population data
-#Make sure that the factor levels and names correspond to your dataframe
+#Make sure that the factor levels and names correspond to your dataframe. 
 target <- list(
   gender = setNames(c(0.50, 0.50), c("female", "male")),
   migration = setNames(c(0.3, 0.7), c("yes", "no"))
@@ -15,6 +15,7 @@ target <- list(
 survey_cleaned<-as.data.frame(survey_cleaned) 
 
 -------------------------------------------------------------------------------
+
 #---raking process
 raking<-anesrake(target,
                  survey_cleaned,
